f0d2ab681784b103815be6f02c090c36c19e2083
vicky <vicky.ungemach95@gmail.com>
2023-01-09 09:58:15 -0800

    HS-38 allow multiple items to be added to cart

--- projects/hotsauce/carts/carts.controller.js
+++ projects/hotsauce/carts/carts.controller.js
@@ -5,32 +5,48 @@ const mongoose = require('mongoose');
 /**
  * Add to cart
  * @header x-auth-token
- * @property {string} req.body.productID
- * @property {number} req.body.quantity
- * @property {array} req.body.items
+ * @property {array} req.body.items [{ productID: string, quantity: number}]
  * @returns cart {}
  */
 async function addToCart(req, res, next) {
     try {
-        let updatedCart = {};
         let cart = await Cart.findOne({ userID: req.userID });
-        let cartItem = cart && cart.items.find(item => item.productID.toString() === req.body.items[0].productID);
 
         // If user doesn't have a cart yet, create new cart and save it to user
         if (!cart) {
             cart = await Cart.create({ userID: req.userID });
-            await User.findByIdAndUpdate(req.userID, { cart: cart._id})
+            await User.findByIdAndUpdate(req.userID, { cart: cart._id })
         }
 
-        // If item already exists in cart, up the quantity
-        if (cartItem) {
-            updatedCart = await Cart.findOneAndUpdate({ "_id": cart._id, "items._id": cartItem._id },
-                { "$set": { "items.$.quantity": cartItem.quantity + req.body.items[0].quantity } }, { new: true });
-        } else {
-            updatedCart = await Cart.findByIdAndUpdate(cart.id, { $push: { items: req.body.items } }, { new: true });
+        const newItems = req.body.items
+            .filter(item => (
+                !cart.items.map(item => item.productID.toString()).includes(item.productID.toString())
+            ))
+
+        const existingItems = req.body.items
+            .filter(item => (
+                cart.items.map(item => item.productID.toString()).includes(item.productID.toString())
+            ))
+            .map(item => (
+                cart.items.find(cartItem => cartItem.productID.toString() === item.productID.toString())
+            ))
+
+        // Existing items: If item already exists in cart, up the quantity
+        if (existingItems && existingItems.length) {
+            for (let i = 0; i < existingItems.length; i++) {
+                const reqItem = req.body.items.find(item => item.productID.toString() === existingItems[i].productID.toString());
+
+                cart = await Cart.findOneAndUpdate({ "_id": cart._id, "items._id": existingItems[i]._id },
+                    { "$set": { "items.$.quantity": existingItems[i].quantity + reqItem.quantity } }, { new: true });
+            }
         }
 
-        res.status(200).json(updatedCart);
+        // New Items
+        if (newItems && newItems.length) {
+            cart = await Cart.findByIdAndUpdate(cart.id, { $push: { items: newItems } }, { new: true });
+        }
+
+        res.status(200).json(cart);
     } catch (err) {
         next(err);
         console.log(err)
@@ -45,11 +61,8 @@ async function addToCart(req, res, next) {
 
 async function getCart(req, res, next) {
     try {
-        let cart = await Cart.findOne({ userID: req.userID });
-
-        if(!cart) {
-            cart = await Cart.create({ userID: req.userID });
-        }
+        let cart = { items: [] };
+        cart = await Cart.findOne({ userID: req.userID });
 
         res.status(200).json(cart);
     } catch (err) {
