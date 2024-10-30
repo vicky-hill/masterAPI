96397d02af4cff41e90ba51675639a1c02e7e239
vicky-hill <vicky.ungemach95@gmail.com>
2024-10-30 06:29:48 -0700

    change dist folder to public

---
+++ public/server.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const port = 4000;
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.send('typescript');
+});
+app.listen(port, () => console.log('running '));
+exports.default = app;
